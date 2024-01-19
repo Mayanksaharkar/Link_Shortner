8cdedc4f8303abfe89aa2b2b30185901ede095c1
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-01-19 19:13:07 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Home from "./components/Home";
 import Modal from "./components/Modal";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="h-screen">
@@ -10,6 +11,7 @@ function App() {
       <ToastContainer position="top-center" autoClose={3000} />
       <Home />
       <Modal />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content font-sans absolute bottom-0">
+      <aside>
+        <p>Copyright © 2024 - All right reserved by Link Shortner</p>
+      </aside>
+    </footer>
+  );
+}
+
+export default Footer
